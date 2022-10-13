65d69282ffdbdfdfe31f892794cf30378441e9ba
Alejo <ajelof2@gmail.com>
2022-10-13 08:27:40 -0500

    Initial 2 commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div class="content">
+    <table>
+      <tbody>
+        <tr v-for="element in todos" :key="element.id">
+          <td>{{element.id}}</td>
+          <td>{{element.codigo}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data(){
+    return{
+      todos:null
+    }
+  },
+  mounted(){
+    this.getTodos();
+  },
+  methods:{
+    getTodos(){
+      axios.get('http://localhost:8000/api/Boletas')
+        .then( response =>{
+          console.log(response)
+          this.todos = response.data
+        }).catch(
+          e => console.log(e)
+        )
+    }
   }
+
+
 }
 </script>
 
